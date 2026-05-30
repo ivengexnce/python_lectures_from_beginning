57e06b3f7ed5674b0d8519dbd1c943e122806640
Meet Maru <meetmaru149@gmail.com>
2026-05-31 01:50:38 +0530

    completed bill splitter

--- small-project/simple-bill-splitter.py
+++ small-project/simple-bill-splitter.py
@@ -1,6 +1,21 @@
 # Simple Bill Splitter
 # (Used in apps like Splitwise)
 
-print("==BILL SPLITTER==")
+print("\n\t==BILL SPLITTER==\n")
 
 #get the total bill amount from the user
+total_bill=float(input("Enter the total bill amount: "))
+
+#get the number of people to split the bill
+num_people=int(input("Enter the no. of people to  split the bill in: "))
+
+#split the bill using formula total_amount_of_bill/no. of ppls ( use the condition no. of ppls > 0 )
+
+if num_people >0:
+    amount_per_person_pays=total_bill/num_people
+    print("Each person gets the contri of: ", amount_per_person_pays)
+
+else:
+    print("You have to pay all bill😂👾")
+
+
